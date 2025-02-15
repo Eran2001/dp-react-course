6d866de3be9f58ac7f6bd6748dea2c3fabd352c3
Eran <eran.hasarali.2001@gmail.com>
2025-02-15 20:23:59 +0530

    add bg to grey

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello</h1>
-      <h2 className="text-6xl font-medium underline w-1">World</h2>
     </>
   );
 }
