5d10c4860edbc7e00c4e684f28d5422e189c0274
Eran <eran.hasarali.2001@gmail.com>
2025-02-15 21:57:35 +0530

    fetch some data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json));
+  }, []);
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <h1 className="text-3xl text-center font-bold underline">Hello</h1>
+      <ul className="m-4">
+        {posts.map((post) => {
+          return (
+            <li key={post.id} className="mb-4 p-4 border border-amber-950">
+              <h2 className="text-xl">{post.title}</h2>
+              <p>{post.body}</p>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
