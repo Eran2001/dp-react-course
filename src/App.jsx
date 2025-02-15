c90134b165c13c3769a282ee9516a6b7dad56a0a
Eran <eran.hasarali.2001@gmail.com>
2025-02-15 20:23:09 +0530

    add some styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello</h1>
+      <h2 className="text-6xl font-medium underline w-1">World</h2>
     </>
   );
 }
