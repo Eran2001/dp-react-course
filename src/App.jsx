fe852207e657471325b1bd18d8f17c3d743c913e
Eran <eran.hasarali.2001@gmail.com>
2025-02-16 13:55:18 +0530

    write some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function App() {
-  const one = [1];
-  const two = [2];
-  const three = [...one, ...two, 3];
-  console.log(three);
+  const [number, setNumber] = useState(0);
 
-  const arrow = (num1, num2, num3) => {
-    console.log(num1 + num2 + num3);
-  };
-
-  arrow(...three);
+  useEffect(() => {
+    const myObj = {
+      one: 1,
+      two: 2,
+    };
+    setNumber(...Object.values(myObj));
+  }, []);
 
   return (
     <>
       <h1 className="text-2xl font-bold underline">Hello</h1>
+      <h2>{number}</h2>
     </>
   );
 }
