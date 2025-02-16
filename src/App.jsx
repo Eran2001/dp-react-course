c8dd2388dedde8eb1414df67ace5f833c8263dce
Eran <eran.hasarali.2001@gmail.com>
2025-02-16 13:08:38 +0530

    test spread operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import { useState, useEffect } from "react";
-
 function App() {
-  const [number, setNumber] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setNumber((prev) => prev + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+  const one = [1];
+  const two = [2];
+  const three = [...one, ...two, 3];
+  console.log(three);
 
   return (
     <>
       <h1 className="text-2xl font-bold underline">Hello</h1>
-      <h2>{number}</h2>
     </>
   );
 }
