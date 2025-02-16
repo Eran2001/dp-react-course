bde551feb937f4461714cdf5f785c8b3df259cf9
Eran <eran.hasarali.2001@gmail.com>
2025-02-16 22:35:45 +0530

    add some divs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ function App() {
   return (
     <>
       <h1 className="text-2xl font-bold underline">Hello</h1>
-      <div className="main"></div>
+      <div className="main">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+      </div>
     </>
   );
 }
