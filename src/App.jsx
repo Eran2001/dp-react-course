0aded7417b5b399768bc4389c833dbddb9cbe19e
Eran <eran.hasarali.2001@gmail.com>
2025-02-16 16:50:44 +0530

    test rest operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { useEffect } from "react";
-import { useState } from "react";
-
 function App() {
-  const [number, setNumber] = useState(0);
-
-  useEffect(() => {
-    const myObj = {
-      one: 1,
-      two: 2,
-    };
-    setNumber(...Object.values(myObj));
-  }, []);
-
   return (
     <>
       <h1 className="text-2xl font-bold underline">Hello</h1>
-      <h2>{number}</h2>
     </>
   );
 }
