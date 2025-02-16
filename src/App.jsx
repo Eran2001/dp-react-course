5f3d3d9ef2562aae66db504dcfe7167d9ae073b7
Eran <eran.hasarali.2001@gmail.com>
2025-02-16 12:50:56 +0530

    test some useEffetct

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNumber((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <h1 className="text-2xl font-bold underline">Hello</h1>
-      <p className="border-2 border-black m-4 p-2 text-justify">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ea
-        impedit consequuntur quod quam aperiam blanditiis omnis, commodi aliquid
-        possimus magnam iure minus culpa rem maiores inventore ab illo neque.
-      </p>
+      <h2>{number}</h2>
     </>
   );
 }
