b403a8d1944c833ef69f514cd72b54de9a5109f5
Eran <eran.hasarali.2001@gmail.com>
2025-02-16 13:24:05 +0530

    write simple arrow funtion using spread

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ function App() {
   const three = [...one, ...two, 3];
   console.log(three);
 
+  const arrow = (num1, num2, num3) => {
+    console.log(num1 + num2 + num3);
+  };
+
+  arrow(...three);
+
   return (
     <>
       <h1 className="text-2xl font-bold underline">Hello</h1>
