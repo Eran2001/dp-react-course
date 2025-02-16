ecf5faff277594205640ed6dbd6c217f4c7221f4
Eran <eran.hasarali.2001@gmail.com>
2025-02-16 11:44:02 +0530

    add some para text

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ function App() {
   return (
     <>
       <h1 className="text-2xl font-bold underline">Hello</h1>
+      <p className="border-2 border-black m-4 p-2 text-justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ea
+        impedit consequuntur quod quam aperiam blanditiis omnis, commodi aliquid
+        possimus magnam iure minus culpa rem maiores inventore ab illo neque.
+      </p>
     </>
   );
 }
