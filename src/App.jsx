3c84ffd71e96cf6e80dbc209eb36d1629334337a
Eran <eran.hasarali.2001@gmail.com>
2025-02-16 23:01:49 +0530

    change h1 position

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-2xl font-bold underline indent-4">Hello</h1>
+      <h1 className="text-2xl font-bold underline indent-4 relative top-2 left-96">
+        Hello
+      </h1>
     </>
   );
 }
