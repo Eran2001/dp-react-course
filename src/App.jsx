8951e565dc9f62e58f71f1be0d91611f855f2a97
Eran <eran.hasarali.2001@gmail.com>
2025-02-16 23:04:38 +0530

    add some margin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-5xl w-46 p-4 bg-amber-500 font-bold underline indent-4 relative top-2 left-96">
+      <h1 className="text-5xl w-46 m-80 p-4 bg-amber-500 font-bold underline indent-4 relative top-2 left-96">
         Hello
       </h1>
     </>
