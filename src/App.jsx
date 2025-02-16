622eb877fe681ffd41e6790e30de0ae9d2d58950
Eran <eran.hasarali.2001@gmail.com>
2025-02-16 22:40:09 +0530

    add some styles for main div

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ function App() {
   return (
     <>
       <h1 className="text-2xl font-bold underline">Hello</h1>
-      <div className="main">
+      <div className="main h-screen flex justify-center items-center gap-6">
         <div className="w-20 h-20 bg-amber-600">1</div>
-        <div className="w-20 h-20 bg-amber-600">2</div>
-        <div className="w-20 h-20 bg-amber-600">3</div>
-        <div className="w-20 h-20 bg-amber-600">4</div>
-        <div className="w-20 h-20 bg-amber-600">5</div>
+        <div className="w-20 h-20 bg-amber-700">2</div>
+        <div className="w-20 h-20 bg-amber-800">3</div>
+        <div className="w-20 h-20 bg-amber-900">4</div>
+        <div className="w-20 h-20 bg-amber-950">5</div>
       </div>
     </>
   );
