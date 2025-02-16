0fe7d4e917929e6b92dc683f802f390e40843c46
Eran <eran.hasarali.2001@gmail.com>
2025-02-16 19:16:39 +0530

    test asnyc

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
 function App() {
+  function my() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+  }
+
+  console.log("Start");
+
+  setTimeout(my, 3000);
+  console.log(5);
+  console.log(6);
+  console.log(7);
+  console.log(8);
+
   return (
     <>
       <h1 className="text-2xl font-bold underline">Hello</h1>
