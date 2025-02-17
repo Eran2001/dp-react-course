3b5057d138e3b5f5371c10e659e3c07688285888
Eran <eran.hasarali.2001@gmail.com>
2025-02-17 20:55:32 +0530

    try simple callback func

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 function App() {
-  function myFirst() {
+  function myFirst(callback) {
     console.log("Hello");
+    callback();
   }
 
   function mySecond() {
     console.log("Goodbye");
   }
 
-  myFirst();
-  mySecond();
+  myFirst(mySecond);
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello</h1>
