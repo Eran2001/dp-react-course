987dce4f553e6f6d40fce3b7c4c9b5455e5216ab
Eran <eran.hasarali.2001@gmail.com>
2025-02-17 11:47:29 +0530

    test asnyc and await

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useEffect } from "react";
+
 function App() {
+  async function openFile() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("File Opened...");
+        resolve();
+      }, 3000);
+    });
+  }
+
+  async function addSomething() {
+    console.log("Something Added...");
+  }
+
+  useEffect(() => {
+    async function init() {
+      await openFile();
+      addSomething();
+    }
+
+    init();
+  }, []);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello</h1>
