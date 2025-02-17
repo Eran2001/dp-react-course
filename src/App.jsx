e89e879f13faee202abb3302e3e9caccf42be21b
Eran <eran.hasarali.2001@gmail.com>
2025-02-17 21:06:18 +0530

    remove some comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { useEffect } from "react";
 
 function App() {
-  // Define the first function that accepts a callback
   function myFirst(callback) {
     callback();
     console.log("Hello");
   }
 
-  // Define an asynchronous function
   const mySecond = () => {
     setTimeout(() => {
       console.log("Goodbye");
     }, 5000);
   };
 
-  // Use the myFirst function with the mySecond callback
   useEffect(() => {
     myFirst(mySecond);
   }, []);
