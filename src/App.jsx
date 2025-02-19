5b92da34ec90852e7ddd7c0ed5126fdb00de7f5c
Eran <eran.hasarali.2001@gmail.com>
2025-02-19 11:21:01 +0530

    test ranodm module

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
 function App() {
+  const items = [
+    "apple",
+    "banana",
+    "cherry",
+    "date",
+    "elderberry",
+    "fig",
+    "grape",
+  ];
+
+  const myFunc = (arr) => {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  };
+
+  const randomItem = myFunc(items);
+  console.log(randomItem);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello</h1>
