fefac2ad0cc86a4a1437f73091e549f392f123f5
Eran <eran.hasarali.2001@gmail.com>
2025-02-19 17:51:45 +0530

    test prop parsing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import PropTypes from "prop-types";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello</h1>
+      <My name="Eran" />
     </>
   );
 }
 
 export default App;
+
+export const My = ({ name }) => {
+  return (
+    <div>
+      <h1 className="text-amber-50">{name}</h1>
+    </div>
+  );
+};
+
+My.propTypes = {
+  name: PropTypes.string,
+};
