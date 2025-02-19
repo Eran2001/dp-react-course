f83b8e59163b8aa49b4827634fb2d87e37ce5f80
Eran <eran.hasarali.2001@gmail.com>
2025-02-19 12:14:05 +0530

    clear all

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 function App() {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const filteredNumbers = numbers.filter((number) => number % 2 == 0);
-
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello</h1>
-      <ul>
-        {filteredNumbers.map((index, number) => (
-          <li key={index}>{number}</li>
-        ))}
-      </ul>
     </>
   );
 }
