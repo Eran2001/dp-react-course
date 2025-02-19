e3c48166c6a0d8355dcf9c1ea37c95cf85419d19
Eran <eran.hasarali.2001@gmail.com>
2025-02-19 09:49:27 +0530

    resets all

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,4 @@
-import { useEffect } from "react";
-
 function App() {
-  function myFirst(callback) {
-    callback();
-    console.log("Hello");
-  }
-
-  const mySecond = () => {
-    setTimeout(() => {
-      console.log("Goodbye");
-    }, 5000);
-  };
-
-  useEffect(() => {
-    myFirst(mySecond);
-  }, []);
-
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello</h1>
