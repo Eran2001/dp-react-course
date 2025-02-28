e3126f3de8ee2cdcfd66d16730e0ea3eaadb2d1a
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 19:02:52 +0530

    test json

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import animals from "./animals";
 function App() {
   const [cat, dog] = animals;
 
-  const { name: catName, sound: catSound } = cat;
-
   return (
     <div className="">
       <h2 className="text-3xl font-bold underline m-4">Hello</h2>
-      <h3>{dog.name}</h3>
-      <h3>{dog.sound}</h3>
-      <h3>{catName}</h3>
-      <h3>{catSound}</h3>
+      <h3>{JSON.stringify(cat)}</h3>
+      <h3>{JSON.stringify(dog)}</h3>
     </div>
   );
 }
