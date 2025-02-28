98e349a7c00ce9dda3f7bbb7d7834578641c511c
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 17:52:28 +0530

    add setInterval function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const now = new Date().toLocaleTimeString();
 
   const [time, setTime] = useState(now);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newTime = new Date().toLocaleTimeString();
+      setTime(newTime);
+    }, 1000); // 1000ms = 1 second
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
 
   const getTime = () => {
     const newTime = new Date().toLocaleTimeString();
