dbd762759f400ca4a97a11dfed874657b6b47694
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 19:00:58 +0530

    change some

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="">
       <h2 className="text-3xl font-bold underline m-4">Hello</h2>
-      <h3>{cat.name}</h3>
+      <h3>{dog.name}</h3>
       <h3>{dog.sound}</h3>
       <h3>{catName}</h3>
       <h3>{catSound}</h3>
