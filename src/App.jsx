57bc867ff1641cd31670dca164859eacd9d46144
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 17:43:04 +0530

    setTime function

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ function App() {
 
   const [time, setTime] = useState(now);
 
-  console.log(now);
+  const getTime = () => {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  };
 
   return (
     <div className="">
-      <h2 className="text-3xl font-bold underline">Hello</h2>
+      <h2 className="text-3xl font-bold underline m-4">{time}</h2>
+      <button onClick={getTime} className="border-2 border-[#000] m-4 p-1">
+        Get Time
+      </button>
     </div>
   );
 }
