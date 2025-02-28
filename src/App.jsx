e1404d4cc4621d5845cf4f0b50b27f567203724c
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 15:28:13 +0530

    add useState function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <h2 className="text-xl font-bold">
+        {isLoggedIn ? "Welcome back, User!" : "Please log in"}
+      </h2>
+      <button
+        onClick={() => setIsLoggedIn(!isLoggedIn)}
+        className="bg-red-500 text-white px-4 py-2 rounded mt-2 ml-2"
+      >
+        Toggle Message
+      </button>
     </>
   );
 }
