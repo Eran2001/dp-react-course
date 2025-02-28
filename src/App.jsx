058dd6f05cdbd42b76755e294f3302dd4eabf179
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 18:51:42 +0530

    test something

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import animals from "./animals";
 
 function App() {
   const [cat, dog] = animals;
-  console.log(cat.sound);
-  console.log(dog.sound);
 
   const { name: catName, sound: catSound } = cat;
-  console.log(catName);
-  console.log(catSound);
 
   return (
     <div className="">
       <h2 className="text-3xl font-bold underline m-4">Hello</h2>
+
+      <h3>{catName}</h3>
+      <h3>{catSound}</h3>
     </div>
   );
 }
