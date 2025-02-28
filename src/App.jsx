939eb309205925f5664db428f040a95bb9705ac8
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 16:53:53 +0530

    remove some

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
   return (
-    <div className="m-4 p-6 border-2 border-black flex flex-col justify-center items-center">
-      <h2 className="text-3xl font-bold">
-        {/* {isLoggedIn ? "Welcome back, User!" : "Please log in"} */}
-        {isLoggedIn && "Welcome back, User!"}
-        {!isLoggedIn && "Please log in"}
-      </h2>
-      <button
-        onClick={() => setIsLoggedIn(!isLoggedIn)}
-        className="bg-red-500 text-white px-4 py-2 rounded mt-2 ml-2"
-      >
-        Toggle Message
-      </button>
+    <div className="">
+      <h2 className="text-3xl font-bold underline">Hello</h2>
     </div>
   );
 }
