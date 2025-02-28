329df5f2dfac50207c39fbcc28f6c6ad7c93f683
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 17:33:56 +0530

    add local time using js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 function App() {
+  const day = new Date().toLocaleTimeString();
+  console.log(day);
+
   return (
     <div className="">
       <h2 className="text-3xl font-bold underline">Hello</h2>
