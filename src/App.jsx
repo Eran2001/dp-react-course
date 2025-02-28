0d5eec9599ab45af0d7569f0d50496085d709d3b
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 17:54:47 +0530

    update something

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,14 @@ function App() {
     const interval = setInterval(() => {
       const newTime = new Date().toLocaleTimeString();
       setTime(newTime);
-    }, 1000); // 1000ms = 1 second
+    }, 1000);
 
-    // Cleanup the interval when the component unmounts
     return () => clearInterval(interval);
   }, []);
-
-  const getTime = () => {
-    const newTime = new Date().toLocaleTimeString();
-    setTime(newTime);
-  };
 
   return (
     <div className="">
       <h2 className="text-3xl font-bold underline m-4">{time}</h2>
-      <button onClick={getTime} className="border-2 border-[#000] m-4 p-1">
-        Get Time
-      </button>
     </div>
   );
 }
