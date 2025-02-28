1484c26178455e68925e2b0cf5eb0b1637c556ad
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 18:38:17 +0530

    test something using array

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import { useState, useEffect } from "react";
+import animals from "./animals";
 
 function App() {
-  const now = new Date().toLocaleTimeString();
-
-  const [time, setTime] = useState(now);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const newTime = new Date().toLocaleTimeString();
-      setTime(newTime);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+  const [x, y] = animals;
+  console.log(x.sound);
+  console.log(y.sound);
 
   return (
     <div className="">
-      <h2 className="text-3xl font-bold underline m-4">{time}</h2>
+      <h2 className="text-3xl font-bold underline m-4">Hello</h2>
     </div>
   );
 }
