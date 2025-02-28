4eaf868c82429b706485fbe493524149db81317a
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 19:45:50 +0530

    remove some

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  const [over, setOver] = useState(false);
-
-  const onOver = () => {
-    setOver(true);
-  };
-
-  const onOut = () => {
-    setOver(false);
-  };
-
   return (
     <div className="">
       <h2 className="text-3xl font-bold underline m-4">Hello</h2>
-      <button
-        onMouseOver={onOver}
-        onMouseOut={onOut}
-        className={`cursor-pointer border-2 border-black m-4 px-4 py-2 ${
-          over ? "bg-black text-white" : "bg-white text-black"
-        }`}
-      >
-        Submit
-      </button>
     </div>
   );
 }
