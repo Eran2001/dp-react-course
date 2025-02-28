cd1d7c9e7ae28b5898dea10f1920980eab94580a
Eran <eran.hasarali.2001@gmail.com>
2025-02-28 19:57:15 +0530

    set input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 function App() {
+  const handleChange = () => {
+    console.log("Changed");
+  };
+
   return (
     <div className="">
-      <h2 className="text-3xl font-bold underline m-4">Hello</h2>
+      <h2 className="text-3xl font-bold underline m-4">Form</h2>
+      <input
+        onChange={handleChange}
+        type="text"
+        name="name"
+        placeholder="Type your name"
+        className="border-2 border-black"
+      />
     </div>
   );
 }
