77d42785e2f86b21484c945f0c0a2252e3f6ccf1
Eran <eran.hasarali.2001@gmail.com>
2025-03-01 10:38:46 +0530

    add input styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
-
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="flex flex-col justify-center items-center h-screen bg-green-600">
+      <h1 className="text-4xl text-white">Hello</h1>
+      <input
+        className="border-2 border-amber-50 text-center px-4 py-2 mt-2"
+        type="text"
+        name="fname"
+        placeholder="First Name"
+      />
+      <input
+        className="border-2 border-amber-50 text-center px-4 py-2 mt-2"
+        type="text"
+        name="lname"
+        placeholder="Last Name"
+      />
+      <input
+        className="border-2 border-amber-50 text-center text-green-500 bg-white px-4 py-2 mt-2 w-54"
+        type="submit"
+        value="Submit"
+      />
     </div>
   );
 };
