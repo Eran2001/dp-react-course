b1187e69b7d7b0c02ba402dd928c6963985527f5
Eran <eran.hasarali.2001@gmail.com>
2025-03-01 11:27:56 +0530

    test ES6 spread operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 const App = () => {
+  const vegetables = [1, 2, 3, 4, 5];
+  const fruits = [...vegetables, 6, 7, 8, 9, 10];
+  console.log(fruits);
+
   return (
     <div>
       <h1 className="text-4xl text-white">Hello</h1>
