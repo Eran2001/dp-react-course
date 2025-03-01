6ab44542ae76ab54bc41b0c890e05e64eae40f5a
Eran <eran.hasarali.2001@gmail.com>
2025-03-01 10:40:15 +0530

    add two state for input names

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 const App = () => {
+  const [fname, setFName] = useState("");
+  const [lname, setLName] = useState("");
+
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-green-600">
-      <h1 className="text-4xl text-white">Hello</h1>
+      <h1 className="text-4xl text-white">
+        Hello {fname} {lname}
+      </h1>
       <input
         className="border-2 border-amber-50 text-center px-4 py-2 mt-2"
         type="text"
