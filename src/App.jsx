efde3db50c92da13f415d42df5d7bcfae76289bc
Eran <eran.hasarali.2001@gmail.com>
2025-03-01 10:41:58 +0530

    add set functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ const App = () => {
   const [fname, setFName] = useState("");
   const [lname, setLName] = useState("");
 
+  const changeFName = (e) => {
+    setFName(e.target.value);
+  };
+
+  const changeLName = (e) => {
+    setFName(e.target.value);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-green-600">
       <h1 className="text-4xl text-white">
@@ -14,12 +22,14 @@ const App = () => {
         type="text"
         name="fname"
         placeholder="First Name"
+        onChange={changeFName}
       />
       <input
         className="border-2 border-amber-50 text-center px-4 py-2 mt-2"
         type="text"
         name="lname"
         placeholder="Last Name"
+        onChange={changeLName}
       />
       <input
         className="border-2 border-amber-50 text-center text-green-500 bg-white px-4 py-2 mt-2 w-54"
