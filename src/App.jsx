be6e4c6e5fdcc437ed15555952cfe0945d3b8edb
Eran <eran.hasarali.2001@gmail.com>
2025-03-01 10:51:45 +0530

    update state to one

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
 import { useState } from "react";
 
 const App = () => {
-  const [fname, setFName] = useState("");
-  const [lname, setLName] = useState("");
+  const [fullname, setFullName] = useState({
+    fName: "",
+    lName: "",
+  });
 
-  const changeFName = (e) => {
-    setFName(e.target.value);
-  };
-
-  const changeLName = (e) => {
-    setLName(e.target.value);
+  const changeFullName = (e) => {
+    setFullName(e.target.value);
   };
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-green-600">
       <h1 className="text-4xl text-white">
-        Hello {fname} {lname}
+        Hello {fullname.fName} {fullname.lName}
       </h1>
       <input
         className="border-2 border-amber-50 text-center px-4 py-2 mt-2"
         type="text"
         name="fname"
         placeholder="First Name"
-        onChange={changeFName}
-        value={fname}
+        onChange={changeFullName}
+        value={fullname.fName}
       />
       <input
         className="border-2 border-amber-50 text-center px-4 py-2 mt-2"
         type="text"
         name="lname"
         placeholder="Last Name"
-        onChange={changeLName}
-        value={lname}
+        onChange={changeFullName}
+        value={fullname.lName}
       />
       <input
         className="border-2 border-amber-50 text-center text-green-700 bg-white px-4 py-2 mt-2 w-54"
