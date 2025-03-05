b9b6d7202511fbd90315ece6d38c986498277793
Eran <eran.hasarali.2001@gmail.com>
2025-03-05 11:39:48 +0530

    add some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold underline m-2 p-2">Hello</h1>
+      <h1 className="text-4xl font-bold underline m-2 p-2 pb-4 text-center border-10 border-black">
+        Hello
+      </h1>
     </div>
   );
 };
