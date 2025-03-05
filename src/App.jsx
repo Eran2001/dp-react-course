f590dcf116ac35a3be5d2b9a0af6c0d056f31b8c
Eran <eran.hasarali.2001@gmail.com>
2025-03-05 11:32:47 +0530

    add some padding and margin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold underline">Hello</h1>
+      <h1 className="text-4xl font-bold underline m-2 p-2 w-full">Hello</h1>
     </div>
   );
 };
