34cd249cb5a5db82659073b9b9354f7307258bbd
Eran <eran.hasarali.2001@gmail.com>
2025-03-05 11:26:42 +0530

    remove js part

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 const App = () => {
-  const vegetables = [1, 2, 3, 4, 5];
-  const fruits = [...vegetables, 6, 7, 8, 9, 10];
-  console.log(fruits);
-
   return (
     <div>
       <h1 className="text-4xl font-bold underline">Hello</h1>
