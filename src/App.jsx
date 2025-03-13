1cb31b07d70052aa1596cb9df12a5b1b923a037d
Eran <eran.hasarali.2001@gmail.com>
2025-03-13 18:54:11 +0530

    add name heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 const App = () => {
+  const name = "John Doe";
+
   return (
     <div>
       <h1 className="text-5xl font-bold underline">Hello, World!</h1>
+      <h3 className="text-3xl font-bold">{name}</h3>
     </div>
   );
 };
