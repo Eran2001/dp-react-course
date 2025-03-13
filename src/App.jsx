be19c9ccd238accf19d4c61ca6d625c0a6f62a35
Eran <eran.hasarali.2001@gmail.com>
2025-03-13 18:55:45 +0530

    add random name

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 const App = () => {
-  const name = "John Doe";
+  const name = ["John", "Doe"];
+  const randomName = name[Math.floor(Math.random() * name.length)];
 
   return (
     <div>
       <h1 className="text-5xl font-bold underline">Hello, World!</h1>
-      <h3 className="text-3xl font-bold">{name}</h3>
+      <h3 className="text-3xl font-bold">{randomName}</h3>
     </div>
   );
 };
