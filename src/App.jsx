1e6e065d35bcc4be93d1c28b25e0598bbc03f39b
Eran <eran.hasarali.2001@gmail.com>
2025-03-14 20:36:38 +0530

    change random num

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 const App = () => {
-  const name = ["John", "Doe"];
-  const randomName = name[Math.floor(Math.random() * name.length)];
-
   return (
     <div>
       <h1 className="text-5xl font-bold underline">Hello, World!</h1>
