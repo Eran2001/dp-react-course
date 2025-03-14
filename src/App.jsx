4073f38345f76df7515818e8bcde2b5337989a61
Eran <eran.hasarali.2001@gmail.com>
2025-03-14 20:39:05 +0530

    add name var

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
+  const name = "Era";
   return (
     <div>
       <h1 className="text-5xl font-bold ">Hello, World!</h1>
+      <h2>My name is {name}</h2>
     </div>
   );
 };
