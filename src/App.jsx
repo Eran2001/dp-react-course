973b7475bdd120a536df4250c5dc85d37c85fd43
Eran <eran.hasarali.2001@gmail.com>
2025-03-14 20:41:53 +0530

    delete some

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 const App = () => {
-  const name = "Era";
-  const old = 24;
   return (
     <div>
       <h1 className="text-5xl font-bold ">Hello, World!</h1>
-      <h2>My name is {name}.</h2>
-      <h3>I&apos;m {old} years old.</h3>
     </div>
   );
 };
